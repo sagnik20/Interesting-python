f850d76c51bc6d5af0ebcc4b03a9c95715b7d43d
Snehashis Sarkar <36337263+snehashis365@users.noreply.github.com>
2020-10-01 03:15:45 +0530

    Fix DisambiguationError, Made query as user input

--- wiki.py
+++ wiki.py
@@ -1,4 +1,15 @@
 import wikipedia
 
-result = wikipedia.summary("Python", sentences=2)
-print(result)
+query = input("Enter Search term: ")
+try:
+    page = wikipedia.page(query)
+    print(page.summary, sentences = 2)
+except:
+    topics = wikipedia.search(query)
+    print(f"Search results for {query} :")
+    for i, topic in enumerate(topics):
+        print (f"{i}. {topic}")
+    choice = int(input("Enter a choice: "))
+    assert choice in range(len(topics))
+    result = wikipedia.summary(topics[choice] , sentences = 2)
+    print(result)
