589180786b75f87e26bf1d1f92cf469f842a425a
ShubhayanS <ss3902@srmist.edu.in>
2020-10-01 12:05:00 +0530

    added input for search

--- image_search.py
+++ image_search.py
@@ -2,16 +2,19 @@ from icrawler.examples import GoogleImageCrawler
 from icrawler.examples import BingImageCrawler
 from icrawler.examples import BaiduImageCrawler
 
+
+a = input("Enter search keyword")
+
 google_crawler = GoogleImageCrawler('./save/')
-google_crawler.crawl(keyword='sunny', offset=0, max_num=10,
+google_crawler.crawl(keyword=a, offset=0, max_num=10,
                      date_min=None, date_max=None, feeder_thr_num=1,
                      parser_thr_num=1, downloader_thr_num=4,
                      min_size=(200,200), max_size=None)
 bing_crawler = BingImageCrawler('./save/')
-bing_crawler.crawl(keyword='sunny', offset=0, max_num=10,
+bing_crawler.crawl(keyword=a, offset=0, max_num=10,
                    feeder_thr_num=1, parser_thr_num=1, downloader_thr_num=4,
                    min_size=None, max_size=None)
 baidu_crawler = BaiduImageCrawler('./save/')
-baidu_crawler.crawl(keyword='sunny', offset=0, max_num=10,
+baidu_crawler.crawl(keyword=a, offset=0, max_num=10,
                     feeder_thr_num=1, parser_thr_num=1, downloader_thr_num=4,
                     min_size=None, max_size=None)
