ac24f31389f5f5566f506bcf285ca97659e14ec3
Snehashis Sarkar <36337263+snehashis365@users.noreply.github.com>
2020-10-01 11:55:02 +0530

    Add Comment Lines

--- wiki.py
+++ wiki.py
@@ -1,15 +1,15 @@
-import wikipedia
+import wikipedia # Please install the python package wikipedia before with "pip3 install wikipedia"
 
-query = input("Enter Search term: ")
-try:
-    page = wikipedia.page(query)
-    print(page.summary, sentences = 2)
-except:
-    topics = wikipedia.search(query)
+query = input("Enter Search term: ") # Prompt the user to enter the search term
+try: # Try block to check for error
+    page = wikipedia.page(query) # Load the page for the search term
+    print(page.summary, sentences = 2) # Print the 1st 2 lines of the summary
+except: # handle error if it occurs
+    topics = wikipedia.search(query) # Load a list of items that match the user input
     print(f"Search results for {query} :")
-    for i, topic in enumerate(topics):
-        print (f"{i}. {topic}")
-    choice = int(input("Enter a choice: "))
-    assert choice in range(len(topics))
-    result = wikipedia.summary(topics[choice] , sentences = 2)
+    for i, topic in enumerate(topics): # Generate a menu from the search results
+        print (f"{i}. {topic}") # Using formated string to display in desired order of index followed by result
+    choice = int(input("Enter a choice: ")) # Prompt user to select desired item by their index
+    assert choice in range(len(topics)) # To avoid IndexOutOfBound Exception
+    result = wikipedia.summary(topics[choice] , sentences = 2) # Stroing the summary in a variable for better code readability
     print(result)
